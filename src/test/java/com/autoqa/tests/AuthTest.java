f288918f4ddaf2279a5000cb33bff257039b8a58
Your Name <your.email@example.com>
2026-04-14 14:30:05 +0530

    Added TestNG DataProvider to fulfill Must-Have Requirement 4.2

--- src/test/java/com/autoqa/tests/AuthTest.java
+++ src/test/java/com/autoqa/tests/AuthTest.java
@@ -5,10 +5,21 @@ import com.autoqa.pages.AuthPage;
 import com.autoqa.pages.ProductPage;
 import com.autoqa.pages.CheckoutPage;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AuthTest extends BaseTest {
 
+    // 1. DATA PROVIDER for Login (Requirement 4.2)
+    @DataProvider(name = "loginData")
+    public Object[][] getLoginData() {
+        return new Object[][] {
+            { "invalid_user_hcl1@mail.com", "WrongPass123" },
+            { "invalid_user_hcl2@mail.com", "WrongPass456" }
+        };
+    }
+
+    // 2. POSITIVE E2E TEST
     @Test(priority = 1)
     public void verifyEndToEndHackathonFlow() {
         AuthPage auth = new AuthPage(driver);
@@ -31,18 +42,18 @@ public class AuthTest extends BaseTest {
         Assert.assertTrue(checkout.isOrderConfirmed(), "Order confirmation was not displayed!");
     }
 
-    @Test(priority = 2)
-    public void verifyLoginWithInvalidCredentials() {
+    // 3. DATA-DRIVEN NEGATIVE TEST (Requirement 4.2 & 3.5)
+    @Test(priority = 2, dataProvider = "loginData")
+    public void verifyLoginWithInvalidCredentials(String userEmail, String userPwd) {
         AuthPage auth = new AuthPage(driver);
         auth.goToAuthPage();
         
-        // Testing negative scenario
-        auth.login("non_existent_user_hcl@mail.com", "WrongPass123");
+        System.out.println(">>> Testing Invalid Login for: " + userEmail);
+        auth.login(userEmail, userPwd);
         
-        // If login fails, we should still be on the login page
         String currentUrl = driver.getCurrentUrl();
         boolean isStillOnAuthPage = currentUrl.contains("login") || currentUrl.contains("signup");
         
-        Assert.assertTrue(isStillOnAuthPage, "System incorrectly allowed login with invalid credentials!");
+        Assert.assertTrue(isStillOnAuthPage, "System allowed login with invalid credentials!");
     }
 }
